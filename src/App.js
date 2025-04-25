cfe98a9bc04705144c95126c03316504bb0bc8f1
Bharat Rathor <bharat.rathor@gemsessence.com>
2025-04-25 15:42:11 +0530

    Playing Arount Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, reset, incrementByAmount } from './features/counter/counterSlice';
+
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const count = useSelector((state) => state.counter.value)
+  const [amount, setAmount] = useState(0)
+  const dispatch = useDispatch();
 
-  const handleClick = () => {
-    setCounter(counter+1);
+  const handleClick = (message) => {
+    message=="plus" ? dispatch(increment()) : dispatch(decrement());
   }
+
+  const resetCounter = () => {
+    dispatch(reset());
+  }
+
+  const handleAmountChange = (amount) => {
+    setAmount(amount);
+  }
+
+  const handleAmountIncrement = () =>{
+    dispatch(incrementByAmount(amount))
+  }
+
   return (
     <div className="App">
-      <button type="button" onClick={handleClick}>Click Me!</button>
-      <h1>Counter: {counter}</h1>
+      <button type="button" onClick={() => handleClick("plus")}>Increase</button>
+      <h1>Counter: {count}</h1>
+      <button type="button" onClick={() => handleClick("minus")}>Decrease</button>
+      <button type="button" onClick={resetCounter}>Reset</button>
+      <input
+        type='number'
+        value={amount}
+        placeholder='Enter Amount'
+        onChange={(e) => handleAmountChange(e.target.value)}
+      >
+        </input>
+        <button type="button" onClick={handleAmountIncrement}>Increment By Amount</button>
+      
     </div>
   );
 }
